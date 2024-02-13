110ecdfc67061fbe25238afe4d7a9a92885cfa2a
Jodhraj Kalawat <42849179+Jodhrajkalawat10@users.noreply.github.com>
2024-02-13 15:18:08 +0530

    Update config.php

--- config.php
+++ config.php
@@ -38,12 +38,12 @@ $CFG = new stdClass();
 // will be stored.  This database must already have been created         //
 // and a username/password created to access it.                         //
 
-$CFG->dbtype    = 'pgsql';      // 'pgsql', 'mariadb', 'mysqli', 'auroramysql', 'sqlsrv' or 'oci'
+$CFG->dbtype    = 'mysqli';      // 'pgsql', 'mariadb', 'mysqli', 'auroramysql', 'sqlsrv' or 'oci'
 $CFG->dblibrary = 'native';     // 'native' only at the moment
-$CFG->dbhost    = 'localhost';  // eg 'localhost' or 'db.isp.com' or IP
-$CFG->dbname    = 'moodle';     // database name, eg moodle
-$CFG->dbuser    = 'username';   // your database username
-$CFG->dbpass    = 'password';   // your database password
+$CFG->dbhost    = 'lovelysunrise-server.mysql.database.azure.com';  // eg 'localhost' or 'db.isp.com' or IP
+$CFG->dbname    = 'lovelysunrise-database';     // database name, eg moodle
+$CFG->dbuser    = 'fsjgdumzqn';   // your database username
+$CFG->dbpass    = '63S176W2080T3V72$';   // your database password
 $CFG->prefix    = 'mdl_';       // prefix to use for all table names
 $CFG->dboptions = array(
     'dbpersist' => false,       // should persistent database connections be
@@ -162,7 +162,7 @@ $CFG->dboptions = array(
 // If you need both intranet and Internet access please read
 // http://docs.moodle.org/en/masquerading
 
-$CFG->wwwroot   = 'http://example.com/moodle';
+$CFG->wwwroot   = 'lovelysunrise.azurewebsites.net';
 
 
 //=========================================================================
@@ -178,7 +178,7 @@ $CFG->wwwroot   = 'http://example.com/moodle';
 //
 // - On Windows systems you might specify something like 'c:\moodledata'
 
-$CFG->dataroot  = '/home/example/moodledata';
+$CFG->dataroot  = 'https://lovelysunrisedata.file.core.windows.net/lovelysunrisedata/moodledata';
 
 
 //=========================================================================
